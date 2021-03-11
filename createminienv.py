6d21fbab1d479ebd88a73d26eb1edbb675bdbd6a
jjjkkkjjj-mizuno <jkado@mizuno.co.jp>
2021-03-11 15:33:48 +0900

    rename createcondaenv.py

--- createminienv.py
+++
@@ -1,9 +0,0 @@
-import glob, os, sys
-
-envs = glob.glob(os.path.join('.', '*.yml'))
-
-for i, env in enumerate(envs):
-    filename = os.path.basename(env)
-    sys.stdout.write('\r Exporting {}: {}/{}'.format(env, i + 1, len(envs)))
-    sys.stdout.flush()
-    _ = os.system('conda env create -f {}'.format(filename))
